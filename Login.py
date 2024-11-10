d2f80942c3f4d0b1455303b70e066050c91e0e54
Assassin-Droid210 <nishat.ahmed1200@gmail.com>
2024-11-10 15:47:12 +0500

    10 November; Added changing username & moved def login()

--- Login.py
+++ Login.py
@@ -1,4 +1,15 @@
-def login(name, password) -> bool:
+def login():
+    tries = 0
+    loginSuccess = False
+    while(tries < 5 and loginSuccess == False):
+        Name = input("Enter user name: ")
+        password = input("Enter password: ")
+        loginSuccess = loginCheck(Name, password)
+        tries += 1
+        if loginSuccess == False:
+            print(f"Wrong name or password, tries left: {5 - tries}")
+
+def loginCheck(name, password) -> bool:
     '''
     Checks if the argument match the values in the mainPass.txt file
     '''
@@ -6,6 +17,7 @@ def login(name, password) -> bool:
         data = file.readlines()
         for line in data:
             word = line.split(',')
+            print(word)
             if word[0] == name and word[1] == password:
                 print("Successfully logged in.")
                 return True
@@ -20,4 +32,14 @@ def changePassword(newPassword):
     
     with open("mainPass.txt", "w") as file:
         appendThis = word[0] + "," + newPassword
+        file.write(appendThis)
+
+def changeUserName(newName):
+    with open("mainPass.txt", "r") as file:
+        data = file.readlines()
+        for line in data:
+            word = line.split(',')
+    
+    with open("mainPass.txt", "w") as file:
+        appendThis =  newName + "," + word[1]
         file.write(appendThis)
