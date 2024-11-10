fddb6884bd813968deb276f740e1f742cf99d7a8
Assassin-Droid210 <nishat.ahmed1200@gmail.com>
2024-11-10 19:29:51 +0500

    10 Nov: Finished adding ADRU file for now

--- addDeleteReadUpdate.py
+++ addDeleteReadUpdate.py
@@ -1,43 +1,86 @@
 import csv
 
-def addEntry(account : str, userName : str, password : str, note : str = "No note.") -> None:
-    '''
-    Adds an entry in the data.csv file.
-    '''
-    listToAppend = [account, userName, password, note]
-    with open('data.csv', 'a', newline = "") as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(listToAppend)
-        
-def readFullFile() -> None:
-    '''
-    Adds an entry as a row in the data.csv file.
-    '''
-    index = 1
-    with open('data.csv', 'r') as csvfile:
-        csv_reader = csv.reader(csvfile)
-        for corpName, userName, password, note in csv_reader:
-            print(f"{index}. {corpName}, {userName}, {password}, {note}")
-            index += 1
+class Add:
+    def addEntry(account : str, userName : str, password : str, note : str = "No note.") -> None:
+        '''
+        Adds an entry in the data.csv file.
+        '''
+        listToAppend = [account, userName, password, note]
+        with open('data.csv', 'a', newline = "") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerow(listToAppend)
 
-def deleteEntryFullInfo(account : str, userName : str, password : str, note : str = "No note.") -> None:
-    '''
-    Deletes either 1 or several rows in the data.csv file.
-    '''
-    listWithRows = []
-    with open('data.csv', 'r') as csvfile:
-        for row in csv.reader(csvfile):
-            if not (row[0] == account and row[1] == userName and row[2] == password and row[3] == note):
-                listWithRows.append(row)
+class Delete:
+    def deleteEntryFullInfo(account : str, userName : str, password : str, note : str = "No note.") -> None:
+        '''
+        Deletes 1 specific entry.
+        '''
+        listWithRows = []
+        with open('data.csv', 'r') as csvfile:
+            for row in csv.reader(csvfile):
+                if not (row[0] == account and row[1] == userName and row[2] == password and row[3] == note):
+                    listWithRows.append(row)
+        
+        with open('data.csv', 'w', newline = "") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(listWithRows)   # Writes rows
     
-    with open('data.csv', 'w', newline = "") as csvfile:
-        csvwriter = csv.writer(csvfile)
-        csvwriter.writerows(listWithRows)   # Writes rows
+    def deleteAllAccount(account : str) -> None:
+        '''
+        Deletes all the rows with account matching "account".
+        '''
+        listWithRows = []
+        with open('data.csv', 'r') as csvfile:
+            for row in csv.reader(csvfile):
+                if not (row[0] == account):
+                    listWithRows.append(row)
+        
+        with open('data.csv', 'w', newline = "") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(listWithRows)   # Writes rows
+            
+    def deleteAllUsers(userName : str) -> None:
+        '''
+        Deletes all the rows with username matching "userName".
+        '''
+        listWithRows = []
+        with open('data.csv', 'r') as csvfile:
+            for row in csv.reader(csvfile):
+                if not (row[1] == userName):
+                    listWithRows.append(row)
+        
+        with open('data.csv', 'w', newline = "") as csvfile:
+            csvwriter = csv.writer(csvfile)
+            csvwriter.writerows(listWithRows)   # Writes rows
+
+class Read:
+    def readFullFile() -> None:
+        '''
+        It reads the whole file and outputs it in the terminal.
+        '''
+        index = 1
+        with open('data.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for corpName, userName, password, note in csv_reader:
+                print(f"{index}. {corpName}, {userName}, {password}, {note}")
+                index += 1
+    
+    def groupSpecialValues(value : str, position : int) -> None:
+        '''
+        It reads rows that have similar account to account parameter and outputs them.
+        '''
+        index = 1
+        with open('data.csv', 'r') as csvfile:
+            csv_reader = csv.reader(csvfile)
+            for row in csv_reader:
+                if value == row[position]:
+                    print(f"{index}. {row[0]}, {row[1]}, {row[2]}, {row[3]}")
+                    index += 1
 
 class Update:
     def updateValue(account : str, userName : str, password : str, valueToUpdate : str, position : int) -> None:
         '''
-        Use Case: to update username, password or note.
+        Update username or password or note.
         '''
         listWithRows = []
         with open('data.csv', 'r') as csvfile:
--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import Login
 import addDeleteReadUpdate as ADRU
 
-ADRU.readFullFile()
-ADRU.readFullFile()
+ADRU.Add.addEntry("Google", "nishat", 'abc')
+ADRU.Add.addEntry("Google", "asdfasf", 'abc')
+ADRU.Add.addEntry("Google", "xcmvnxc", 'abc')
+ADRU.Add.addEntry("Google", "asodfjasd", 'abc')
+ADRU.Add.addEntry("Facebook", "asodfjasd", 'abc')
+ADRU.Read.readFullFile()
+ADRU.Read.groupSpecialValues("Google", 0)
